3d764030adaaa92f61e2e4cb008fc5d7c401667d
Shane Gilbert <shanegilbert@Shanes-MacBook-Pro.local>
2023-10-15 11:59:48 -0700

    added google apis to react

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,20 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+
 export function App() {
+  <GoogleOAuthProvider>
   return (
     <div>
       <h1>Your website here</h1>
+      <GoogleLogin>
+        onSuccess={credentialResponse => {
+        console.log(credentialResponse);
+      }}
+      onError={() => {
+        console.log('Login Failed');
+  }}
+      </GoogleLogin>
     </div>
   )
+  </GoogleOAuthProvider>
 }
