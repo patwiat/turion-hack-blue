c8154c8809a832ccd84fe467a8618224d1bc308a
Shane Gilbert <shanegilbert@Shanes-MacBook-Pro.local>
2023-10-15 14:35:08 -0700

    added google oauth. needs styling

--- public/src/App.js
+++ public/src/App.js
@@ -1,14 +1,17 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { useGoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider, GoogleLogin, useGoogleLogin } from '@react-oauth/google';
+import GoogleButton from 'react-google-button'
+import jwtDecode from 'jwt-decode';
 import SolarSystem from "./pages/SolarSystem";
 
 export function App() {
-  console.log("hello");
   return (
     <div>
       <SolarSystem />
-      <GoogleOAuthProvider clientID="606620997861-bd8qmbcvsg6400s2ok7votrfhe4qgk8s.apps.googleusercontent.com"></GoogleOAuthProvider>
+      <GoogleOAuthProvider clientId="606620997861-bd8qmbcvsg6400s2ok7votrfhe4qgk8s.apps.googleusercontent.com">
+        <GoogleLogin onSuccess={credentialResponse => {
+          console.log(credentialResponse)
+        }}/>;
+      </GoogleOAuthProvider>
     </div>
   );
-  // </GoogleOAuthProvider>;
 }
