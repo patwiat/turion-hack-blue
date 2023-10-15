2df8bb4c2bc7948ee1687e31065506a416fd7463
Gabriel Gibb <gabrielgibb1@gmail.com>
2023-10-15 14:46:56 -0700

    add mongo db connection

--- app.js
+++ app.js
@@ -50,30 +50,53 @@ server.listen(PORT, undefined, undefined, () => {
 
 //Mongo DB
 
-// require('dotenv').config();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://shanedgilbert:" + process.env.SHANE_ID + "@cluster0.kncph0j.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://shanedgilbert:" + process.env.SHANE_ID + "@cluster0.kncph0j.mongodb.net/?retryWrites=true&w=majority";
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+var database = client.db("Planets");
+var journeys = database.collection("journeys");
+var chatLogs = database.collection("chatLogs");
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+
+async function viewChatLog(planet){
+    var logs = await chatLogs.findOne();
+    console.log(logs[planet]);
+}
+
+
+// async function addMessage(planet, userId, msg){
+//     var logs = await chatLogs.findOne('$elemMatch', {'earth' : chatLogs})
+//     console.log(logs)
 // }
 
-// run().catch(console.dir);
+// addMessage('earth', 'testUser2', 'testMessage2')
+
+// planets = ['earth', 'moon', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto', 'sun', 'asteroid belt']
+// async function addPlanets(){
+
+// }
