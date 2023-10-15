90d77b781bacca888382e71d3f5fd6e60ef3b5db
msclal <mika1shanela@gmail.com>
2023-10-15 13:39:54 -0700

    setup tailwind and pages

--- public/src/index.js
+++ public/src/index.js
@@ -1,39 +1,46 @@
 // Accessible from anywhere
-window.IS_DEV = process.env.NODE_ENV === 'development';
+window.IS_DEV = process.env.NODE_ENV === "development";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { GoogleLogin } from '@react-oauth/google'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GoogleLogin } from "@react-oauth/google";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
 export const socket = (() => {
-  const SERVER_URL = 'http://localhost:3000'
+  const SERVER_URL = "http://localhost:3000";
   const socket = io(SERVER_URL);
-  
-  socket.on('connect', () => {
-    console.log('Established socket.io connection with server!')
+
+  socket.on("connect", () => {
+    console.log("Established socket.io connection with server!");
   });
-  
+
   return socket;
 })();
 
-import { App } from './App'
+<GoogleLogin
+  onSuccess={(credentialResponse) => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log("Login Failed");
+  }}
+/>;
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>
-  }
-])
+    path: "/",
+    element: <App />,
+  },
+]);
 
-createRoot(document.getElementById('app')).render(
+createRoot(document.getElementById("app")).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
-)
+);
