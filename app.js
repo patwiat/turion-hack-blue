0b81e2828ba96a581f364ccbe108a7c43aefaf70
Suneel Freimuth <suneelfreimuth1@gmail.com>
2023-10-15 15:34:13 -0700

    Integrate chat with mongodb on server

--- app.js
+++ app.js
@@ -25,61 +25,7 @@ app.get('/video/:planet', (req, res) =>{
     res.sendFile(__dirname + '/videos/' + req.params['planet'] + '.mp4');
 });
 
-//Socket IO
 const server = createServer(app);
-
-const socketio = require("socket.io");
-const io = new socketio.Server(server, {
-    cors: {
-        // TODO: Change to client domain at some point
-        origin: `*`
-    }
-});
-
-const CHAT_ROOMS = {
-    sun: 'sun',
-    mercury: 'mercury',
-    venus: 'venus',
-    earth: 'earth',
-    moon: 'moon',
-    mars: 'mars',
-    asteroid_belt: 'asteroid_belt',
-    jupiter: 'jupiter',
-    saturn: 'saturn',
-    uranus: 'uranus',
-    neptune: 'neptune',
-    pluto: 'pluto',
-};
-
-io.on('connection', socket => {
-    console.log(`New connection! (${socket.id}`);
-
-    socket.on('join_room', ({ room }) => {
-        console.assert(room in CHAT_ROOMS);
-        for (const room of Array.from(socket.rooms.values()))
-            if (room !== socket.id)
-                socket.leave(room);
-        socket.join(room);
-        console.log(`${socket.id} joined room "${room}"`)
-    });
-
-    socket.on('chat_message', ({ sender, content }) => {
-        // Should only be one room: the object the user is currently at.
-        socket.rooms.forEach(room => {
-            if (room === socket.id)
-                return;
-            console.log('Sending to room', room)
-            socket.to(room).emit('chat_message', { sender, content });
-        });
-    });
-});
-
-const PORT = 3000
-
-server.listen(PORT, undefined, undefined, () => {
-    console.log(`http://localhost:${PORT}/`);
-});
-
 
 //Mongo DB
 
@@ -100,18 +46,16 @@ var journeys = database.collection("journeys");
 var chatLogs = database.collection("chatLogs");
 
 async function run() {
-  try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
 }
+
+process.on('exit', async () => {
+    await client.close();
+});
 
 run().catch(console.dir);
 
@@ -133,3 +77,62 @@ async function viewChatLog(planet){
 // async function addPlanets(){
 
 // }
+
+// socket.io
+
+const socketio = require("socket.io");
+const io = new socketio.Server(server, {
+    cors: {
+        // TODO: Change to client domain at some point
+        origin: `*`
+    }
+});
+
+const CHAT_ROOMS = new Set([
+    'sun',
+    'mercury',
+    'venus',
+    'earth',
+    'moon',
+    'mars',
+    'asteroid belt',
+    'jupiter',
+    'saturn',
+    'uranus',
+    'neptune',
+    'pluto',
+]);
+
+io.on('connection', socket => {
+    console.log(`New connection! (${socket.id}`);
+
+    socket.on('join_room', ({ room }) => {
+        console.assert(CHAT_ROOMS.contains(room));
+        for (const room of Array.from(socket.rooms.values()))
+            if (room !== socket.id)
+                socket.leave(room);
+        socket.join(room);
+        console.log(`${socket.id} joined room "${room}"`)
+    });
+
+    socket.on('chat_message', async ({ sender, content }) => {
+        // Should only be one room: the object the user is currently at.
+        socket.rooms.forEach(async room => {
+            if (room === socket.id)
+                return;
+            await chatLogs.updateOne({ planet: room }, {
+                $push: {
+                    'chatLogs': { sender, content }
+                }
+            });
+            socket.to(room).emit('chat_message', { sender, content });
+        });
+    });
+});
+
+
+const PORT = 3000
+
+server.listen(PORT, undefined, undefined, () => {
+    console.log(`http://localhost:${PORT}/`);
+});
