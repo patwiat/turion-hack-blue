793a85e664ccb5c3bbd3684a283268573d5d7bab
Shane Gilbert <shanegilbert@Shanes-MacBook-Pro.local>
2023-10-15 15:19:03 -0700

    updated google oauth

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 app.use(express.static('public/dist'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const { createServer } = require('http');
 
